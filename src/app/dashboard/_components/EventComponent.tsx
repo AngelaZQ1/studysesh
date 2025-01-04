60485fffa4a03da81364a127e2aaad4c3ded232a
AngelaZQ1 <zheng.ang@northeastern.edu>
2025-01-04 10:28:39 -0500

    remove unnecessary Flex, flexShrink, include sesh if user is participant

--- src/app/dashboard/_components/EventComponent.tsx
+++ src/app/dashboard/_components/EventComponent.tsx
@@ -1,11 +1,11 @@
-// import useUserContext from "@/app/_hooks/useUserContext";
+import useUserContext from "@/app/_hooks/useUserContext";
 import { Event } from "@/app/_types/types";
-import { Popover, Text, Box, Stack, Flex } from "@mantine/core";
+import { Popover, Text, Box, Stack, Flex, Button } from "@mantine/core";
 import moment from "moment";
-import { FiCalendar, FiClock, FiMapPin } from "react-icons/fi";
+import { FiCalendar, FiClock, FiEdit3, FiMapPin } from "react-icons/fi";
 
 const EventComponent = ({ event }: { event: Event }) => {
-  // const { userId } = useUserContext();
+  const { userId } = useUserContext();
 
   return (
     <Popover withArrow position="top" withinPortal>
@@ -19,31 +19,25 @@ const EventComponent = ({ event }: { event: Event }) => {
         <Flex align="end" mt="xs" wrap="nowrap">
           <Stack gap={4}>
             <Flex gap={3} align="center">
-              <Flex w={20} h={20} justify="center" align="center">
-                <FiCalendar color="gray" size={16} />
-              </Flex>
-              <Text size="sm" c="gray.7">
+              <FiCalendar color="gray" size={16} />
+              <Text size="sm" c="gray.7" sx={{ flexShrink: "0" }}>
                 {moment(event.start).format("dddd, MMMM D")}
               </Text>
             </Flex>
             <Flex gap={3} align="center">
-              <Flex w={20} h={20} justify="center" align="center">
-                <FiClock color="gray" size={16} />
-              </Flex>
+              <FiClock color="gray" size={16} />
               <Text size="sm" c="gray.7">
                 {moment(event.start).format("h:mma")}
               </Text>
             </Flex>
             <Flex gap={3} align="center">
-              <Flex w={20} h={20} justify="center" align="center">
-                <FiMapPin color="gray" size={16} />
-              </Flex>
+              <FiMapPin color="gray" size={16} style={{ flexShrink: "0" }} />
               <Text size="sm" c="gray.7">
                 {event.virtual ? "Virtual" : event.location}
               </Text>
             </Flex>
           </Stack>
-          {/* {event.ownerId === userId ? (
+          {event.ownerId === userId ? (
             <Button
               variant="outline"
               color="gray.5"
@@ -55,7 +49,7 @@ const EventComponent = ({ event }: { event: Event }) => {
             >
               Edit Sesh
             </Button>
-          ) : null} */}
+          ) : null}
         </Flex>
       </Popover.Dropdown>
     </Popover>
