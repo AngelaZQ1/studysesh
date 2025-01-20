e4e63986fb20b5795fd31f19fbf8154a50cb3786
AngelaZQ1 <zheng.ang@northeastern.edu>
2025-01-19 22:59:22 -0500

    show dashboard if logged in

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,10 @@
+"use client";
 import { default as LogIn } from "./login/page";
+import { default as Dashboard } from "./dashboard/page";
+import useUserContext from "./_hooks/useUserContext";
 
 export default function Home() {
-  return <LogIn />;
+  const { firebaseUser } = useUserContext();
+
+  return firebaseUser ? <Dashboard /> : <LogIn />;
 }
