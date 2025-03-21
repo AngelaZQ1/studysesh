130022238b5fa889d807193cdfa116e473fa2bd4
AngelaZQ1 <zheng.ang@northeastern.edu>
2025-03-21 15:09:13 -0400

    useSesh -> redux

--- src/app/seshSlice.ts
+++ src/app/seshSlice.ts
@@ -42,6 +42,56 @@ export const fetchAllSeshes = createAsyncThunk(
   }
 );
 
+export const createSesh = createAsyncThunk(
+  "seshes/createSesh",
+  async (data: { newSesh; idToken: string }) => {
+    try {
+      await fetch("/api/sesh", {
+        method: "POST",
+        body: JSON.stringify({ ...data.newSesh }),
+        headers: {
+          Authorization: `Bearer ${data.idToken}`,
+        },
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  }
+);
+
+export const updateSesh = createAsyncThunk(
+  "seshes/updateSesh",
+  async (data: { id: number; updatedSesh; idToken: string }) => {
+    try {
+      await fetch(`/api/sesh/${data.id}`, {
+        method: "PUT",
+        body: JSON.stringify({ ...data.updatedSesh }),
+        headers: {
+          Authorization: `Bearer ${data.idToken}`,
+        },
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  }
+);
+
+export const deleteSesh = createAsyncThunk(
+  "seshes/deleteSesh",
+  async (data: { id: number; idToken: string }) => {
+    try {
+      await fetch(`/api/sesh/${data.id}`, {
+        method: "DELETE",
+        headers: {
+          Authorization: `Bearer ${data.idToken}`,
+        },
+      });
+    } catch (err) {
+      console.error(err);
+    }
+  }
+);
+
 // export const { setSeshes } = seshSlice.actions;
 
 export default seshSlice.reducer;
