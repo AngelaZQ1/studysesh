beab22f56d18a13c4c9da12e6ca3ce1ca643cd36
AngelaZQ1 <zheng.ang@northeastern.edu>
2025-03-21 14:46:13 -0400

    usePusher

---
+++ src/app/_hooks/usePusher.ts
@@ -0,0 +1,29 @@
+import { Sesh } from "@prisma/client";
+import { useEffect } from "react";
+import { pusherClient } from "../pusher";
+import { fetchAllSeshes } from "../seshSlice";
+import useUserContext from "./useUserContext";
+import { useDispatch } from "react-redux";
+
+const usePusher = () => {
+  const { firebaseUser, user } = useUserContext();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    pusherClient.subscribe("public");
+
+    pusherClient.bind("new-sesh", (sesh: Sesh) => {
+      if (sesh.participants.some((p) => p.id === user.id)) {
+        firebaseUser.getIdToken().then((idToken) => {
+          dispatch(fetchAllSeshes(idToken));
+        });
+      }
+    });
+
+    return () => {
+      pusherClient.unsubscribe("public");
+    };
+  }, []);
+};
+
+export default usePusher;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,33 +1,19 @@
 "use client";
+import { useRouter } from "next/navigation";
 import { useEffect } from "react";
-import useSesh from "./_hooks/useSesh";
 import useUserContext from "./_hooks/useUserContext";
-import { Sesh } from "./_types/types";
-import { default as Dashboard } from "./dashboard/page";
-import { default as LogIn } from "./login/page";
-import { pusherClient } from "./pusher";
 
 export default function Home() {
-  const { firebaseUser, user } = useUserContext();
-  const { seshes, fetchSeshes } = useSesh();
+  const { firebaseUser } = useUserContext();
+  const router = useRouter();
 
   useEffect(() => {
-    pusherClient.subscribe("public");
+    if (firebaseUser) {
+      router.push("/dashboard");
+    } else {
+      router.push("/login");
+    }
+  }, [firebaseUser, router]);
 
-    pusherClient.bind("new-sesh", (sesh: Sesh) => {
-      if (sesh.participants.some((p) => p.id === user.id)) {
-        fetchSeshes();
-      }
-    });
-
-    return () => {
-      pusherClient.unsubscribe("public");
-    };
-  }, []);
-
-  return firebaseUser ? (
-    <Dashboard seshes={seshes} fetchSeshes={fetchSeshes} />
-  ) : (
-    <LogIn />
-  );
+  return null;
 }
