0516082a45fa132257e9e1e791e3accac17ce8ba
AngelaZQ1 <zheng.ang@northeastern.edu>
2025-03-24 11:45:53 -0400

    dashboard link in nav

--- src/app/_components/NavBar.tsx
+++ src/app/_components/NavBar.tsx
@@ -1,14 +1,15 @@
 "use client";
 
-import { Avatar, Button, Flex, Menu, Text } from "@mantine/core";
+import { Anchor, Avatar, Button, Flex, Menu, Text } from "@mantine/core";
+import { notifications } from "@mantine/notifications";
+import { getAuth, signOut } from "firebase/auth";
 import Image from "next/image";
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { FiChevronDown, FiLogOut, FiUser } from "react-icons/fi";
 import logo from "../../../public/studyseshLogo.svg";
 import useUserContext from "../_hooks/useUserContext";
-import { FiChevronDown, FiLogOut } from "react-icons/fi";
-import { getAuth, signOut } from "firebase/auth";
-import { notifications } from "@mantine/notifications";
 import classes from "./NavBar.module.css";
-import { useRouter } from "next/navigation";
 
 export default function NavBar() {
   const router = useRouter();
@@ -42,41 +43,54 @@ export default function NavBar() {
 
   return (
     <nav className={classes.nav}>
-      <Flex px={30} py={5} pt={8} justify="space-between" gap="md">
-        <Image src={logo} alt="logo" width={40} />
+      <Flex px={30} py={10} pt={8} justify="space-between" gap="md">
+        <Image src={logo} alt="logo" width={45} />
 
-        {user && (
-          <Flex align="center">
-            <Avatar color="pink" radius="xl" size={30}>
-              {user.firstName[0].toUpperCase() + user.lastName[0].toUpperCase()}
-            </Avatar>
+        <Flex align="center" gap={50}>
+          <Link href="/dashboard">
+            <Anchor size="md" c="gray.7" underline="hover">
+              Dashboard
+            </Anchor>
+          </Link>
+          {user && (
+            <Flex align="center">
+              <Avatar color="pink" radius="xl" size={30}>
+                {user.firstName[0].toUpperCase() +
+                  user.lastName[0].toUpperCase()}
+              </Avatar>
 
-            <Menu shadow="md" width={150} trigger="hover">
-              <Menu.Target>
-                <Button
-                  ml={5}
-                  px={10}
-                  variant="subtle"
-                  color="black"
-                  rightSection={<FiChevronDown />}
-                >
-                  <Text size="xs" fw={500}>
-                    {user.firstName} {user.lastName[0]}
-                  </Text>
-                </Button>
-              </Menu.Target>
-              <Menu.Dropdown>
-                <Menu.Item onClick={navigateProfilePage}>Profile</Menu.Item>
-                <Menu.Item
-                  leftSection={<FiLogOut size={14} />}
-                  onClick={handleLogOut}
-                >
-                  Log Out
-                </Menu.Item>
-              </Menu.Dropdown>
-            </Menu>
-          </Flex>
-        )}
+              <Menu shadow="md" trigger="hover">
+                <Menu.Target>
+                  <Button
+                    ml={5}
+                    px={10}
+                    variant="white"
+                    color="black"
+                    rightSection={<FiChevronDown />}
+                  >
+                    <Text size="md" fw={500}>
+                      {user.firstName} {user.lastName[0]}
+                    </Text>
+                  </Button>
+                </Menu.Target>
+                <Menu.Dropdown>
+                  <Menu.Item
+                    leftSection={<FiUser size={14} />}
+                    onClick={navigateProfilePage}
+                  >
+                    Profile
+                  </Menu.Item>
+                  <Menu.Item
+                    leftSection={<FiLogOut size={14} />}
+                    onClick={handleLogOut}
+                  >
+                    Log Out
+                  </Menu.Item>
+                </Menu.Dropdown>
+              </Menu>
+            </Flex>
+          )}
+        </Flex>
       </Flex>
     </nav>
   );
