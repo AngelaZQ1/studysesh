15fe45b12a573daee0c1df4bf503e094e3c4c374
Angela Zheng <92692008+AngelaZQ1@users.noreply.github.com>
2025-05-21 16:26:54 -0400

    profile page and profileSesh

--- src/app/profile/[id]/page.tsx
+++ src/app/profile/[id]/page.tsx
@@ -3,14 +3,16 @@
 import useUser from "@/app/_hooks/useUser";
 import useUserContext from "@/app/_hooks/useUserContext";
 import { Sesh, User } from "@/app/_types/types";
-import { fetchAllSeshes } from "@/app/seshSlice";
+import ProfileSesh from "@/app/profile/_components/ProfileSesh";
+import { fetchSeshesForCurrentUser } from "@/app/seshSlice";
 import {
   Avatar,
   Box,
   Center,
   Container,
-  Divider,
   Flex,
+  Loader,
+  Stack,
   Text,
   TextInput,
 } from "@mantine/core";
@@ -25,6 +27,7 @@ export default function Page() {
 
   const { getUserById } = useUser();
   const [user, setUser] = useState<User | null>(null);
+  const [isLoading, setIsLoading] = useState(true);
 
   const router = useRouter();
   const pathname = usePathname();
@@ -39,23 +42,26 @@ export default function Page() {
   const isCurrentUser = currentUser && currentUser.id === userId;
 
   useEffect(() => {
-    const fetchUser = async () => {
-      const res = await getUserById({ id: userId }).then((res) => {
+    const fetchUserAndSeshes = async () => {
+      const fetchUser = async () => {
+        const res = await getUserById({ id: userId });
         if (res.error) {
           setUser(null);
         } else {
           setUser(res);
         }
-      });
-    };
-    const fetchSeshes = async () => {
-      firebaseUser.getIdToken().then((idToken) => {
-        dispatch(fetchAllSeshes(idToken));
-      });
+      };
+
+      const fetchSeshes = async () => {
+        const idToken = await firebaseUser.getIdToken();
+        await dispatch(fetchSeshesForCurrentUser(idToken));
+      };
+
+      await Promise.all([fetchUser(), fetchSeshes()]);
+      setIsLoading(false);
     };
 
-    fetchUser();
-    fetchSeshes();
+    fetchUserAndSeshes();
   }, []);
 
   const futureSeshes = seshes.filter(
@@ -65,53 +71,63 @@ export default function Page() {
     (sesh: Sesh) => new Date(sesh.start) < new Date()
   );
 
-  return user === null ? (
-    <Center>
-      <Text size="xl" c="gray.7" mt={30} fw={700}>
-        This user doesnt exist!
-      </Text>
-    </Center>
-  ) : (
-    <Container size="sm" mt={100}>
-      {isCurrentUser ? (
-        <>
-          <Avatar color="pink" radius="xl" size={50} mb={20}>
-            {currentUser.firstName[0] + currentUser.lastName[0]}
-          </Avatar>
-          <Flex gap={10}>
-            <TextInput label="First" placeholder={currentUser.firstName} />
-            <TextInput label="Last" placeholder={currentUser.lastName} />
-          </Flex>
-
-          <Box mt={50}>
-            <Text size="xl">Upcoming Seshes</Text>
-          </Box>
-          {futureSeshes.map((sesh) => (
-            <Box key={sesh.id} mt={10}>
-              <Text size="sm">{sesh.title}</Text>
-              <Divider />
-            </Box>
-          ))}
-
-          <Box mt={50}>
-            <Text size="xl">Past Seshes</Text>
-          </Box>
-          {pastSeshes.map((sesh) => (
-            <Box key={sesh.id} mt={10}>
-              <Text size="sm">{sesh.title}</Text>
-              <Divider />
-            </Box>
-          ))}
-        </>
-      ) : (
-        user && (
-          <Flex align="center" gap={10}>
-            <Avatar color="pink" radius="xl" size={50}>
-              {user.firstName[0].toUpperCase() + user.lastName[0].toUpperCase()}
+  return (
+    <Container size={550} mt={100}>
+      {isLoading ? (
+        <Center>
+          <Loader mr="sm" /> Fetching Profile...
+        </Center>
+      ) : user === null ? (
+        <Text size="xl" c="gray.7" mt={30} fw={700}>
+          This user doesnt exist!
+        </Text>
+      ) : isCurrentUser ? (
+        <Stack>
+          <Box>
+            <Avatar color="pink" radius="xl" size={50} mb={20}>
+              {currentUser.firstName[0] + currentUser.lastName[0]}
             </Avatar>
-            <Text size="xl">{user.firstName + " " + user.lastName}</Text>
-          </Flex>
-        )
+            <Flex gap={10}>
+              <TextInput
+                label="First Name"
+                placeholder={currentUser.firstName}
+                flex="1"
+              />
+              <TextInput
+                label="Last Name"
+                placeholder={currentUser.lastName}
+                flex="1"
+              />
+            </Flex>
+          </Box>
+
+          <Box mt="xl">
+            <Text size="xl" mb="sm" fw={700}>
+              Upcoming Seshes
+            </Text>
+            {futureSeshes.map((sesh: Sesh) => (
+              <ProfileSesh sesh={sesh} />
+            ))}
+          </Box>
+
+          <Box>
+            <Text size="xl" mb="sm" fw={700}>
+              Past Seshes
+            </Text>
+            <Stack gap={0}>
+              {pastSeshes.map((sesh: Sesh) => (
+                <ProfileSesh sesh={sesh} />
+              ))}
+            </Stack>
+          </Box>
+        </Stack>
+      ) : (
+        <Flex align="center" gap={10}>
+          <Avatar color="pink" radius="xl" size={50}>
+            {user.firstName[0].toUpperCase() + user.lastName[0].toUpperCase()}
+          </Avatar>
+          <Text size="xl">{user.firstName + " " + user.lastName}</Text>
+        </Flex>
       )}
     </Container>
   );
---
+++ src/app/profile/_components/ProfileSesh.tsx
@@ -0,0 +1,86 @@
+import useUserContext from "@/app/_hooks/useUserContext";
+import { Sesh } from "@/app/_types/types";
+import { Avatar, Box, Card, Flex, Stack, Text } from "@mantine/core";
+import moment from "moment";
+import { FiCalendar, FiClock, FiMapPin } from "react-icons/fi";
+
+export default function ProfileSesh({ sesh }: { sesh: Sesh }) {
+  const { user } = useUserContext();
+  return (
+    <Card shadow="xs" padding="lg" mb="sm" radius="md" withBorder>
+      <Flex key={sesh.id} justify="space-between" align="start" gap={10}>
+        <Stack gap="2">
+          <Text fw={700}>{sesh.title}</Text>
+          <Stack gap="3">
+            <Flex gap={3} align="center" w="max-content">
+              <FiCalendar color="gray" size={16} />
+              <Text size="sm" c="gray.7" sx={{ flexShrink: "0" }}>
+                {moment(sesh.start).format("dddd, MMMM D")}
+              </Text>
+            </Flex>
+            {sesh.time && (
+              <Flex gap={3} align="center">
+                <FiClock color="gray" size={16} />
+                <Text size="sm" c="gray.7">
+                  {moment(sesh.time, "HH:mm").format("h:mmA")}
+                </Text>
+              </Flex>
+            )}
+            <Flex gap={3} align="center">
+              <FiMapPin color="gray" size={16} style={{ flexShrink: "0" }} />
+              <Text size="sm" c="gray.7">
+                {sesh.virtual ? "Virtual" : sesh.location}
+              </Text>
+            </Flex>
+          </Stack>
+        </Stack>
+        <Stack gap="2" w="30%">
+          <Box>
+            <Text size="sm" mb={4}>
+              Owner
+            </Text>
+            <Flex align="center" gap="xs">
+              {sesh.owner.id === user.id ? (
+                <Avatar size="sm" color="pink">
+                  {sesh.owner.firstName[0].toUpperCase() +
+                    sesh.owner.lastName[0].toUpperCase()}
+                </Avatar>
+              ) : (
+                <Avatar size="sm">
+                  {sesh.owner.firstName[0].toUpperCase() +
+                    sesh.owner.lastName[0].toUpperCase()}
+                </Avatar>
+              )}
+              <Text size="sm">
+                {sesh.owner.firstName + " " + sesh.owner.lastName}
+              </Text>
+            </Flex>
+          </Box>
+          {sesh.participants.length > 0 && (
+            <Box mt="md">
+              <Text size="sm" mb={4}>
+                Participants
+              </Text>
+              {sesh.participants.map((p) => (
+                <Flex align="center" gap="xs">
+                  {p.id === user.id ? (
+                    <Avatar size="sm" color="pink">
+                      {p.firstName[0].toUpperCase() +
+                        p.lastName[0].toUpperCase()}
+                    </Avatar>
+                  ) : (
+                    <Avatar size="sm">
+                      {p.firstName[0].toUpperCase() +
+                        p.lastName[0].toUpperCase()}
+                    </Avatar>
+                  )}
+                  <Text size="sm">{p.firstName + " " + p.lastName}</Text>
+                </Flex>
+              ))}
+            </Box>
+          )}
+        </Stack>
+      </Flex>
+    </Card>
+  );
+}
