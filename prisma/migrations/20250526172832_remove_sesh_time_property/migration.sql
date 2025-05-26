77b48428b8de17397d8b8c43107a0dd8d2516f27
Angela Zheng <92692008+AngelaZQ1@users.noreply.github.com>
2025-05-26 14:39:32 -0400

    prisma migration

---
+++ prisma/migrations/20250526172832_remove_sesh_time_property/migration.sql
@@ -0,0 +1,8 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `time` on the `Sesh` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Sesh" DROP COLUMN "time";
